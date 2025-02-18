549541dff731f0547ddbe2c5c0a59aa71a0c399d
Auxology <akaki.jomidavayt@gmail.com>
2025-02-18 12:16:47 +0400

    Project init

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+if(!port) {
+  process.exit(1);
+}
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
